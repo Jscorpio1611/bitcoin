78bc86711a8e6ef3e43402c42d84874161ad0033
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-11 04:28:59 +0000

    Add new bitcoin_rw.conf file that is used for settings modified by this software itself

--- src/util/system.h
+++ src/util/system.h
@@ -41,6 +41,7 @@
 int64_t GetStartupTime();
 
 extern const char * const BITCOIN_CONF_FILENAME;
+extern const char * const BITCOIN_RW_CONF_FILENAME;
 
 void SetupEnvironment();
 bool SetupNetworking();
@@ -86,6 +87,7 @@ bool CheckDataDirOption();
 /** Tests only */
 void ClearDatadirCache();
 fs::path GetConfigFile(const std::string& confPath);
+fs::path GetRWConfigFile(const std::string& confPath);
 #ifdef WIN32
 fs::path GetSpecialFolderPath(int nFolder, bool fCreate = true);
 #endif
@@ -139,6 +141,8 @@ struct SectionInfo
     std::string m_file;
     int m_line;
 };
+
+void ModifyRWConfigStream(std::istream& stream_in, std::ostream& stream_out, const std::map<std::string, std::string>& settings_to_change);
 
 class ArgsManager
 {
@@ -198,6 +202,9 @@ protected:
      */
     std::vector<util::SettingsValue> GetSettingsList(const std::string& arg) const;
 
+private:
+    fs::path rwconf_path GUARDED_BY(cs_args);
+
 public:
     ArgsManager();
     ~ArgsManager();
@@ -209,6 +216,10 @@ public:
 
     NODISCARD bool ParseParameters(int argc, const char* const argv[], std::string& error);
     NODISCARD bool ReadConfigFiles(std::string& error, bool ignore_invalid_keys = false);
+
+    void ModifyRWConfigFile(const std::map<std::string, std::string>& settings_to_change);
+    void ModifyRWConfigFile(const std::string& setting_to_change, const std::string& new_value);
+    void EraseRWConfigFile();
 
     /**
      * Log warnings for options in m_section_only_args when
