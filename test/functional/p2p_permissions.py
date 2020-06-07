69d5ae93eedc50249e0b78b292ab94d4beb543de
Luke Dashjr <luke-jr+git@utopios.org>
2020-06-07 22:48:26 +0000

    Include "blockfilters" in NetPermissions::ToStrings as appropriate

--- test/functional/p2p_permissions.py
+++ test/functional/p2p_permissions.py
@@ -89,7 +89,7 @@ class P2PPermissionsTests(BitcoinTestFramework):
         self.checkpermission(
             # all permission added
             ["-whitelist=all@127.0.0.1"],
-            ["forcerelay", "noban", "mempool", "bloomfilter", "relay"],
+            ["blockfilters", "forcerelay", "noban", "mempool", "bloomfilter", "relay"],
             False)
 
         self.stop_node(1)
